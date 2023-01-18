95c684e3c4fea873adfd7ec693bcda148d3ee575
isaferjim <95497147+isaferjim@users.noreply.github.com>
2023-01-18 19:18:36 +0100

    Update Subsequence.java

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -15,6 +15,37 @@ public class Subsequence {
     @SuppressWarnings("rawtypes")
     public boolean find(List x, List y) {
         // TODO: Implement the logic here
-        return false;
+
+        // check if the list is create
+        if (x == null || y == null) throw new IllegalArgumentException();
+
+        // check if x is larger than list. If it is true, it can not be subsequence of Y
+
+        if (x.size() > y.size()) {
+            return false;
+        }
+
+        // check if the list X is empty. If it is true, it will always be subsequence of Y
+        if (x.isEmpty()) {
+            return true;
+        }
+
+        // check if y contains x.
+        List<Integer> indiceY= new ArrayList();
+
+        for (int i = 0; i < x.size(); i++) {
+            int indice = y.indexOf(x.get(i));
+            indiceY.add(indice);
+        }         
+                
+        //
+        for (int i=0; i<indiceY.size()-1; i++){
+            if ( indiceY.get(i) > indiceY.get(i+1) ){
+                return false;
+            }
+        }             
+
+
+        return true;
     }
 }
