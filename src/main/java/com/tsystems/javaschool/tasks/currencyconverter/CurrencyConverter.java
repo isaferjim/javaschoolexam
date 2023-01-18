4c6322eb9581834e044f52090b3edfe6fa9347c4
isaferjim <95497147+isaferjim@users.noreply.github.com>
2023-01-18 20:06:29 +0100

    Update CurrencyConverter.java

--- src/main/java/com/tsystems/javaschool/tasks/currencyconverter/CurrencyConverter.java
+++ src/main/java/com/tsystems/javaschool/tasks/currencyconverter/CurrencyConverter.java
@@ -11,6 +11,52 @@ public class CurrencyConverter {
      */
     public String convert(double dollarToEuroRate, String statement) {
         // TODO: Implement the logic here
+        
+        String money1 = null;
+        String money2 = null; 
+        
+
+
+        if (statement.isEmpty()) {
+            throw new CannotConvertCurrencyException();}
+
+        if (statement.contains("convertToDollar")){
+            if (statement.contains("$")){
+                throw new CannotConvertCurrencyException();
+            }
+
+            
+
+        }else if (statement.contains("convertToEuro")) {
+              if(statement.contains("euro")){
+                throw new CannotConvertCurrencyException();
+            }
+            
+         } else {
+            throw new CannotConvertCurrencyException();}
+
+            
+            if (statement.contains("+")){
+                String [] parts= statement.split("+");
+                String money1 = parts[0];
+                String money2 = parts[1];
+                
+    
+            }
+
+
+
+
+
         return "";
+    }
+
+    public static int operacion(statement op, int n1, int n2){
+        switch (op){
+            case '+':
+                return n1 + n2;
+            case '-':
+                return n1 - n2;    
+        }
     }
 }
